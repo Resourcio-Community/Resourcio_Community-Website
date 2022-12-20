a403329c0889e0c926a702af043043a38322c9d8
ABHIRUP <abhirupsaha02@gmail.com>
2022-12-20 20:02:21 +0530

    .

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
+const authRoute = require('./routes/auth')
+
+dotenv.config()
+
+mongoose.set('strictQuery', true)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('frontend'))
+app.use(express.json())
+app.use('/api/auth', authRoute)
+
+
+
+const port = process.env.PORT || 3000
+mongoose.connect("mongodb://localhost:27017/resourcioDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Database Connected')
+    app.listen(port, () => {
+        console.log(`Server connected: ${port}`)
+    })
+})
+    .catch((error) => {
+        console.log(error.message)
+    })
---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+# Resourcio Community-Website
