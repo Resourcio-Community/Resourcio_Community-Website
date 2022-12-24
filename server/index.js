04639b31b883786ef49731622fcd158eeaafb1b8
ABHIRUP <abhirupsaha02@gmail.com>
2022-12-24 10:28:04 +0530

    .

--- server/index.js
+++ server/index.js
@@ -9,35 +9,21 @@ dotenv.config()
 
 mongoose.set('strictQuery', true)
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('frontend'))
 app.use(express.json())
 app.use('/api/auth', authRoute)
 
 
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/frontend/index.html')
-})
-
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/frontend/login.html')
-})
-
-app.get('/resources', (req, res) => {
-    res.sendFile(__dirname + '/frontend/resource.html')
-})
-
-
 const port = process.env.PORT || 3000
-mongoose.connect("mongodb://localhost:27017/resourcioDB", {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => {
-    console.log('Database Connected')
-    app.listen(port, () => {
-        console.log(`Server connected: ${port}`)
-    })
 })
-    .catch((error) => {
-        console.log(error.message)
+    .then(() => {
+        console.log('Database Connected')
+        app.listen(port, () => {
+            console.log(`Server connected: ${port}`)
+        })
     })
+    .catch((error) =>{
+    console.log(error.message)
+})
