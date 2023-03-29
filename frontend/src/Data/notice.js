0d0206fd93dd487106e86934121cf3ebc48efdc5
ABHIRUP <abhirupsaha02@gmail.com>
2023-03-29 22:11:27 +0530

    .

--- frontend/src/Data/notice.js
+++ frontend/src/Data/notice.js
@@ -1,13 +1,17 @@
 const UpcomingEvents = [
     {
         id: 1,
-        event: 'Cloud Computing Session with Ronit Banerjee',
+        event: 'Cloud Computing Session with Ronit Banerjee at 9 p.m.',
         link: 'https://www.w3schools.com/css/'
     },
     {
         id: 2,
         event: 'Cryptography Session at 9 p.m. today with Raja Majumder',
         link: 'https://www.w3schools.com/css/'
+    },
+    {
+        id: 3,
+        event: 'AI/ML session with Subhro'
     }
 ]
 
