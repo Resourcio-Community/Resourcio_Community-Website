c62e106c2f28e2d39a4d75e019f65144a26fbbef
ABHIRUP <abhirupsaha02@gmail.com>
2023-04-07 20:44:13 +0530

    getevents route send res in reverse order

--- server/controllers/eventControllers.js
+++ server/controllers/eventControllers.js
@@ -41,7 +41,7 @@ export async function getEvents(req, res) {
     try {
         const events = await Event.find()
 
-        res.status(200).json(events)
+        res.status(200).json(events.reverse())
     }
     catch (error) {
         res.status(500).json(error)
