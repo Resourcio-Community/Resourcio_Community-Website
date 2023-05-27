3cfc68500418084129b5f8f82c3ab40f0875495f
Sattwikmaiti <maitisattwik@gmail.com>
2023-05-27 23:53:58 +0530

    Resource Card is designed in the Resource scence page

--- frontend/src/component/resourceCard/ResourceCard.jsx
+++ frontend/src/component/resourceCard/ResourceCard.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../../authContext/AuthContext'
 import { useHistory } from 'react-router-dom'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import 'react-lazy-load-image-component/src/effects/blur.css'
-
+import PersonIcon from '@mui/icons-material/Person';
 
 const ResourceCard = ({ image, title, content, href }) => {
 
@@ -16,7 +16,7 @@ const ResourceCard = ({ image, title, content, href }) => {
     }
 
     return (
-        <div className="box_card">
+        /*<div className="box_card">
             <figure className="box_img">
                 <LazyLoadImage
                     className='image'
@@ -30,6 +30,77 @@ const ResourceCard = ({ image, title, content, href }) => {
                 <p className="resource-text">{content}</p>
             </div>
         </div>
+    */
+   /*<div className="box_card">
+    <div className="cardimage">
+        <div className="new">
+            NEW
+        </div>
+        <div className="image">
+        <img src={image} alt={`${title}`} />
+        </div>
+        
+    </div>
+    <div className="description">
+        <div className="title">
+            {title}
+        </div>
+        <div className="shortdesc">
+            {content?.substring(0,100)}
+        </div>
+        <div className="button">
+            <button className="btn"  onClick={handleEvent}>Learn More</button>
+        </div>
+    </div>
+
+   </div>
+
+    */
+
+
+   <div className="card">
+    <div className="new">
+        NEW
+    </div>
+    <div className="imgbox">
+
+    </div>
+
+   <div className="content">
+           <span className="enrolled">
+             <PersonIcon className="icon" />
+              70 enrolled
+            </span> 
+            <div className="title">
+                {title}
+            </div>
+            <div className="shortdescription">
+                {content?.substring(0,70)}. . .
+            </div>
+            <div className="smallcategory">
+                <div className="cat">
+                    beginner
+                </div>
+                <div className="cat">
+                    intermediate
+                </div>
+                <div className="cat">
+                    job-friendly
+                </div>
+            </div>
+            <div className="buttons">
+                <div className="flex">
+                <button class="button">Learn More</button>
+                </div>
+                
+            </div>
+
+    </div>
+   </div>
+
+
+
+
     )
 }
 
