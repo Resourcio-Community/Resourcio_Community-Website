ee4435c5ec90e80db571eed0c5d2cbf0c6ca2ffd
Harsh Upadhye <harshupadhye2832@gmail.com>
2023-06-05 17:10:14 +0530

    Dynamic Style

--- frontend/src/component/stats/Stat.jsx
+++ frontend/src/component/stats/Stat.jsx
@@ -1,18 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import './stat.css'
+import './stat.css';
 import NumberCounter from 'number-counter';
 
 const Stat = ({ style, fontSize, cardTitle, cardText }) => {
+  const [delay, setDelay] = useState(null);
 
-    return (
-        <div className="stats-card" style={{ "--color": `${style}` }}>
-            <h3 className="card-title" >
-                <NumberCounter end={cardTitle} start={0} delay={6} />  + 
-            </h3>
-            <p className="card-text"><span style={{ "fontSize": `${fontSize}px` }}>{cardText}</span></p>
+  useEffect(() => {
+    const handleScroll = () => {
+      const cardElement = document.getElementById('stats-card');
+      const cardPosition = cardElement.getBoundingClientRect();
+      const shouldStartAnimation = cardPosition.top <= window.innerHeight * 0.8; 
 
-        </div>
-    )
-}
+      if (shouldStartAnimation && delay === null) {
+        setDelay(6); 
+      }
+    };
 
-export default Stat
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [delay]);
+
+  return (
+    <div className="stats-card" style={{ "--color": `${style}` }} id="stats-card">
+      <h3 className="card-title">
+        {delay !== null ? (
+          <NumberCounter end={cardTitle} start={0} delay={delay} />
+        ) : (
+          cardTitle
+        )}
+        +
+      </h3>
+      <p className="card-text">
+        <span style={{ fontSize: `${fontSize}px` }}>{cardText}</span>
+      </p>
+    </div>
+  );
+};
+
+export default Stat;
+
