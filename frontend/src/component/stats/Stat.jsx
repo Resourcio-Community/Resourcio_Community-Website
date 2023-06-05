1909303526571a0621f806ef3a54a1b3deed2c04
ABHIRUP <abhirupsaha02@gmail.com>
2023-06-05 17:37:41 +0530

    changed delay time of stat box

--- frontend/src/component/stats/Stat.jsx
+++ frontend/src/component/stats/Stat.jsx
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import './stat.css';
-import NumberCounter from 'number-counter';
+import React, { useState, useEffect } from 'react'
+import './stat.css'
+import NumberCounter from 'number-counter'
 
 const Stat = ({ style, fontSize, cardTitle, cardText }) => {
-  const [delay, setDelay] = useState(null);
+  const [delay, setDelay] = useState(null)
 
   useEffect(() => {
     const handleScroll = () => {
-      const cardElement = document.getElementById('stats-card');
-      const cardPosition = cardElement.getBoundingClientRect();
+      const cardElement = document.getElementById('stats-card')
+      const cardPosition = cardElement.getBoundingClientRect()
       const shouldStartAnimation = cardPosition.top <= window.innerHeight * 0.8; 
 
       if (shouldStartAnimation && delay === null) {
-        setDelay(6); 
+        setDelay(3)
       }
-    };
+    }
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll)
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [delay]);
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [delay])
 
   return (
     <div className="stats-card" style={{ "--color": `${style}` }} id="stats-card">
