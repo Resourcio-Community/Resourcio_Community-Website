a2f4606e32ae15540e4f17647efd086d1d2770e9
ABHIRUP <abhirupsaha02@gmail.com>
2023-06-24 03:29:15 +0530

    vercel/analytics added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Analytics } from '@vercel/analytics/react'
 import axios from 'axios'
 import Spinner from './component/spinner/Spinner'
 import { Route, Switch, Redirect } from "react-router-dom"
@@ -19,27 +20,31 @@ axios.defaults.baseURL = process.env.REACT_APP_API_URL
 const App = () => {
 
   const { user } = useContext(AuthContext)
-  
+
   return (
-    <Suspense fallback={<Spinner width='40px' height='40px' />}>
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route exact path='/resources'>
-          <Resources />
-        </Route>
-        <Route exact path='/proglang'>
-          {user ? <Proglang /> : <Redirect to='/login' />}
-        </Route>
-        <Route exact path='/login'>
-          {!user ? <Login /> : <Redirect to='/' />}
-        </Route>
-        <Route path='*'>
-          <NotFound />
-        </Route>
-      </Switch>
-    </Suspense>
+    <>
+      <Suspense fallback={<Spinner width='40px' height='40px' />}>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/resources'>
+            <Resources />
+          </Route>
+          <Route exact path='/proglang'>
+            {user ? <Proglang /> : <Redirect to='/login' />}
+          </Route>
+          <Route exact path='/login'>
+            {!user ? <Login /> : <Redirect to='/' />}
+          </Route>
+          <Route path='*'>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
+      
+      <Analytics />
+    </>
   )
 }
 
