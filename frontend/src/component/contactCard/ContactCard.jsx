dfdcb40138d3c71567717d2567d4fcdb8dce6617
ABHIRUP <abhirupsaha02@gmail.com>
2023-09-10 21:25:32 +0530

    removed lazy loading

--- frontend/src/component/contactCard/ContactCard.jsx
+++ frontend/src/component/contactCard/ContactCard.jsx
@@ -1,14 +1,9 @@
 import './contactCard.css'
-import { LazyLoadImage } from 'react-lazy-load-image-component'
-import 'react-lazy-load-image-component/src/effects/blur.css'
-
 
 const ContactCard = ({ image, name, role, linkedIn }) => {
     return (
         <div className="contact_card">
-            <div className='dev_image'>
-                <LazyLoadImage src={image} style={{transition:'all 0.3s ease'}} effect='blur' alt={name} />
-            </div>
+            <img src={image} className='dev_image' alt={name} />
             <h3 className='dev_name'>{name}</h3>
             <p className="dev_role">{role}</p>
             <a href={linkedIn} target='_blank' className='dev_profile'>View Profile</a>
