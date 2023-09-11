4129290a5438152042cf79de90ac90db36389e60
ABHIRUP <abhirupsaha02@gmail.com>
2023-09-11 18:49:48 +0530

    updated to react 18

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import { AuthContextProvider } from './context/authContext/AuthContext'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeContextProvider } from './context/ThemeContext'
 
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <BrowserRouter>
     <ThemeContextProvider>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
     </ThemeContextProvider>
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 )
